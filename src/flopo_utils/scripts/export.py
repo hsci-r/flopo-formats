3214ce8d5bb2ccf319de0f1787cdc9d03ab67d56
Maciej Janicki <maciej.janicki@helsinki.fi>
2020-02-24 12:09:50 +0200

    Updated and simplified flopo-export
    - updated to new data structures
    - support cross-sentence annotations
    - removed the CLI options `--text` and `--linearize` (better handled in R)

--- src/flopo_utils/scripts/export.py
+++ src/flopo_utils/scripts/export.py
@@ -8,7 +8,7 @@ from flopo_utils.data import Corpus
 import flopo_utils.io
 
 
-def export_document(doc, writer, doc_id, layer, header=False, text=False, linearize=[]):
+def export_document(doc, writer, doc_id, layer, header=False):
     features = None
     for l, f in doc.schema:
         if l == layer:
@@ -18,25 +18,13 @@ def export_document(doc, writer, doc_id, layer, header=False, text=False, linear
         raise Exception('Annotation layer {} not found'.format(layer))
     if header:
         writer.writerow(
-            ('articleId', 'sentenceId', 'startWordId', 'endWordId')\
-            + tuple(f for f in features if f) \
-            + (('text',) if text else ()) \
-            + tuple('{}.{}'.format(l, f) for l, f in linearize))
-    for s_id, s in enumerate(doc.sentences, 1):
-        if layer in s.spans:
-            for (start, end, values) in s.spans[layer]:
-                row = (doc_id, s_id, start, end) \
-                      + tuple(values[f] for f in features if f)
-                if text:
-                    row += (''.join([t.string + t.space_after \
-                            for t in s.tokens[start-1:end]]).strip(),)
-                if linearize:
-                    for l, f in linearize:
-                        relevant_spans = \
-                            [sp for sp in s.spans[l] if sp[0] >= start and sp[1] <= end]
-                        relevant_spans.sort()
-                        row += (' '.join(str(sp[2][f]) for sp in relevant_spans),)
-                writer.writerow(row)
+            ('articleId', 'startSentenceId', 'startWordId', 'endSentenceId',
+             'endWordId')\
+            + tuple(f for f in features if f))
+    for a in doc.annotations[layer]:
+        row = (doc_id, a.start_sen, a.start_tok, a.end_sen, a.end_tok) \
+              + tuple(a[f] for f in features if f)
+        writer.writerow(row)
 
 
 def parse_arguments():
@@ -56,13 +44,6 @@ def parse_arguments():
         '-a', '--annotation', metavar='LAYER',
         help='The annotation layer to export.')
     parser.add_argument(
-        '-t', '--text', action='store_true',
-        help='Attach the text value of each annotation span.')
-    parser.add_argument(
-        '-l', '--linearize', nargs='+',
-        help='Attach the value of some other features for each annotation'\
-             ' span. Supply a list of entries of the form: LAYER.FEATURE.')
-    parser.add_argument(
         '-d', '--delimiter', default=',',
         help='Delimiter to separate the fields.')
     parser.add_argument(
@@ -79,14 +60,12 @@ def main():
     if args.output_file is not None and args.output_file != '-':
         outfp = open(args.output_file, 'w+')
     writer = csv.writer(outfp, delimiter=args.delimiter)
-    linearize = [x.split('.') for x in args.linearize] if args.linearize is not None else []
     if args.input_file is not None:
         doc = flopo_utils.io.load_webanno_tsv(args.input_file)
         doc_id = args.doc_id
         if doc_id is None:
             doc_id = os.path.basename(args.input_file).replace('.tsv', '')
-        export_document(doc, writer, doc_id, args.annotation, header=first,
-            text=args.text, linearize=linearize)
+        export_document(doc, writer, doc_id, args.annotation, header=first)
         first = False
     if args.input_dir is not None:
         for dirpath, dirnames, filenames in os.walk(args.input_dir):
@@ -94,7 +73,7 @@ def main():
                 doc_id = f.replace('.tsv', '')
                 doc = flopo_utils.io.load_webanno_tsv(os.path.join(dirpath, f))
                 export_document(doc, writer, doc_id, args.annotation,
-                    header=first, text=args.text, linearize=linearize)
+                                header=first)
                 first = False
     outfp.close()
 
